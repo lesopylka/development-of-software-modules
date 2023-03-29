6ddd597a030bfba2a175910159f53b6aeb6cea1c
Alina <99533520+lesopylka@users.noreply.github.com>
2023-03-29 20:05:08 +0300

    formed optimization skills

---
+++ 28-29.cs
@@ -0,0 +1,35 @@
+﻿//Используя метод «золотого сечения», составить программу нахождения
+//наименьшего значения заданной функции с точностью 10^(-5)
+
+using System;
+
+namespace Code {
+  class Program {
+    static void Main(string[] args) {
+      double start = 1;
+      double end = 3;
+      double eps = 0.00001;
+      double z = (3 - Math.Sqrt(5)) / 2;
+      double x1 = start + z * (end - start);
+      double x2 = end - z * (end - start);
+
+      for (int i = 0; end - start > eps; i++) {
+        if (Function(x1) <= Function(x2)) {
+          end = x2;
+          x2 = x1;
+          x1 = start + end - x2;
+        } else {
+          start = x1;
+          x1 = x2;
+          x2 = start + end - x1;
+        }
+      }
+      double x = (start + end) / 2;
+      Console.WriteLine(x);
+      Console.WriteLine($"x: {x}\nFunction(x): {Function(x)}");
+    }
+
+    private static double Function(double x) =>
+      (Math.Pow(x, 2) / 2) - (1 / x);
+  }
+}
