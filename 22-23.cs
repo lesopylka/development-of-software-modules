e9a33e0307fb8443fe6166ed975b0085f663d97f
Alina <99533520+lesopylka@users.noreply.github.com>
2023-03-30 03:44:05 +0300

    UPD:law is used

--- 22-23.cs
+++ 22-23.cs
@@ -1,49 +1,47 @@
 ﻿using System;
 class HelloWorld {
-  static void Main() {
+  static void Main(string[] args) {
     Console.WriteLine("Enter sequence length: ");
 
     int length = Convert.ToInt32(Console.ReadLine());
-    int value = 0;
-    int count = 0;
-    int sum = 0;
+    DoubleNegativeNumbers(length);
+    DevideBy7(length);
+    DevideBy5NotBy7(length);
+  }
 
-    Console.WriteLine("Doubled odd numbers are: ");
+  public static void DoubleNegativeNumbers(int length) {
+    Console.Write("Doubled odd numbers are: ");
 
     for (int i = 1; i < length + 1; i++) {
-      if (i % 2 == 0) {
-        value = i / 2;
-      }
-      if (value % 2 != 0) {
-        Console.WriteLine(i);
+      if (i % 2 == 0 && i / 2 % 2 != 0) {
+        Console.Write($"{i}");
       }
     }
+  }
+
+  public static void DevideBy7(int length) {
+
     Console.WriteLine("when divided by 7 gives a remainder of 1, 2 or 5: ");
     for (int i = 0; i < length + 1; i++) {
-      if (i % 7 == 1) {
-        Console.WriteLine(i);
+      if (i % 7 == 1 || i % 7 == 2 || i % 7 == 5) {
+        Console.Write($"{i}");
       }
-      if (i % 7 == 2) {
-        Console.WriteLine(i);
-      }
-      if (i % 7 == 5) {
-        Console.WriteLine(i);
-      }
-
     }
+  }
 
+  public static void DevideBy5NotBy7(int length) {
+    int value = 0;
+    int sum = 0;
     Console.WriteLine("are divisible by 5 and not divisible by 7, find the number and sum of those members of the sequence");
 
     for (int i = 0; i < length + 1; i++) {
-      if (i % 5 == 2) {
-        if (i % 7 != 0) {
-          count++;
-          sum += i;
-        }
+      if (i % 5 == 2 && i % 7 != 0) {
+        value++;
+        sum += i;
       }
     }
-    Console.WriteLine(count);
+
+    Console.WriteLine(value);
     Console.WriteLine(sum);
   }
 }
-
